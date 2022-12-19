4971e1aa1aeb442f275128e7772e83a3863402b5
dmdwns2 <wjsdncl321@gmail.com>
2022-12-19 10:31:44 +0900

    RsData ChatMessage 추가

--- src/main/java/com/ll/exam/app_2022_12_19__1/test/ChatController.java
+++ src/main/java/com/ll/exam/app_2022_12_19__1/test/ChatController.java
@@ -1,4 +1,4 @@
-package com.ll.exam.app_2022_12_19__1.controller;
+package com.ll.exam.app_2022_12_19__1.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ChatController {
     @PostMapping("/writeMessage")
     @ResponseBody
-    public String writeMessage(){
-        return "메시지가 작성되었습니다.";
+    public RsData<ChatMessage> writeMessage() {
+        ChatMessage message = new ChatMessage("홍길동", "안녕하세요.");
+
+        return new RsData(
+                "S-1",
+                "메세지가 작성되었습니다.",
+                message
+        );
     }
 }
